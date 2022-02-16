61d3876072558ed3702ec2273818c300ac0fa9a6
Ewoodss <70382659+Ewoodss@users.noreply.github.com>
2022-02-16 10:24:24 +0100

    Added lcd
    added lcd and some default includes

---
+++ Week3/Week3/lcd.h
@@ -0,0 +1,3 @@
+void init();
+void display_text(char *str);
+void set_cursor(int position);
--- Week3/Week3/main.c
+++ Week3/Week3/main.c
@@ -5,7 +5,20 @@
  *  Author: Ewout
  */ 
 
+
+#define F_CPU 8e6
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include "lcd.h"
 #include <xc.h>
+
+void wait( int ms ) {
+	for (int i=0; i<ms; i++) {
+		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
+	}
+}
+
 
 int main(void)
 {
