3dbae9c3a43bfa366f331e886697329a8e3b3ccb
Ewoodss <70382659+Ewoodss@users.noreply.github.com>
2022-02-16 10:20:28 +0100

    Week3
    Setup

---
+++ Week3/Week3/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 2/16/2022 10:19:11 AM
+ *  Author: Ewout
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
