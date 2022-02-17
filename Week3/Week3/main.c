05bf4180c8de9b6728d3f8ce506221e16202e9ea
Leon van der Sar <leon.v.d.sar@gmail.com>
2022-02-17 09:43:03 +0100

    OpdrachtB2

--- Week3/Week3/main.c
+++ Week3/Week3/main.c
@@ -24,20 +24,66 @@ void wait( int ms ) {
 	}
 }
 
-volatile int TimerPreset = 0;  // 0xF6, 10 till overflow
-// Interrupt routine timer2 overflow
-ISR( TIMER2_OVF_vect ) {
-	TCNT2 = TimerPreset;	// Preset value
+
+void timer2Init( void ) {
+	//OCR2 = 250;				// Compare value of counter 2
+	TIMSK |= BIT(7);		// T2 compare match interrupt enable
+	sei();					// turn_on intr all
+	TCCR2 = 0b00001101;		// Initialize T2: timer, prescaler=1024, compare output disconnected,CTC,RUN
 }
+
+volatile int TimerPreset1 = -195;
+volatile int TimerPreset2 = -117;
+volatile int state = 0;
+ISR( TIMER2_COMP_vect ) {
+	if (state) {
+		TCNT2 = TimerPreset1;
+		PORTD ^= BIT(7);
+		state = !state;
+		
+	} else {
+		TCNT2 = TimerPreset2;
+		PORTD ^= BIT(7);
+		state = !state;
+	}
+}
+
+int main( void ) {
+	DDRD = 0xFF;
+	DDRC = 0xFF;					// set PORTC for output (toggle PC0)
+	timer2Init();
+
+	while (1) {
+		// do something else
+		PORTC = TCNT2;
+		wait(10);			// every 10 ms (busy waiting
+	}
+}
+
+//------------------------------------------- Opdracht B1
+
+// wait(): busy waiting for 'ms' millisecond
+// Used library: util/delay.h
+//void wait( int ms ) {
+//	for (int tms=0; tms<ms; tms++) {
+//		_delay_ms( 1 );			// library function (max 30 ms at 8MHz)
+//	}
+//}
+
+//volatile int TimerPreset = 0;  // 0xF6, 10 till overflow
+// Interrupt routine timer2 overflow
+//ISR( TIMER2_OVF_vect ) {
+//	TCNT2 = TimerPreset;	// Preset value
+//}
 
 // Initialize timer2
-void timer2Init( void ) {
-	TIMSK |= BIT(6);		// T2 overflow interrupt enable
-	sei();				// turn_on intr all
-	TCCR2 = 0x07;		// Initialize T2: ext.counting, rising edge, run
-}
+//void timer2Init( void ) {
+// 	TIMSK |= BIT(6);		// T2 overflow interrupt enable
+//	sei();				// turn_on intr all
+//	TCCR2 = 0x07;		// Initialize T2: ext.counting, rising edge, run
+//}
 
-int main(void) {
+int opdrachtb1(void) {
 	
 	DDRD &= ~BIT(7);		// PD7 op input: DDRD=xxxx xxx0
 	DDRA = 0xFF;			// set PORTA for output (shows countregister)
