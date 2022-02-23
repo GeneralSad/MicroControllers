4d1e9480cbabe204f8d4ae5160ca9979db7764fe
Leon van der Sar <leon.v.d.sar@gmail.com>
2022-02-23 11:37:56 +0100

    Week4

---
+++ Week4/Week4/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 2/23/2022 11:33:21 AM
+ *  Author: leonv
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
