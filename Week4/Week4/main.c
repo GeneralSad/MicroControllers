f1f01af3dabf7036704061903249fdd5cc7eeae4
Leon van der Sar <leon.v.d.sar@gmail.com>
2022-02-23 11:43:45 +0100

    OpdrachtB1

--- Week4/Week4/main.c
+++ Week4/Week4/main.c
@@ -7,10 +7,33 @@
 
 #include <xc.h>
 
-int main(void)
+// Main program: ADC at PF1
+int main( void )
 {
-    while(1)
-    {
-        //TODO:: Please write your application code 
-    }
+	DDRF = 0x00;				// set PORTF for input (ADC)
+	DDRA = 0xFF;				// set PORTA for output
+	DDRB = 0xFF;				// set PORTB for output
+	adcInit();					// initialize ADC
+
+	while (1)
+	{
+		PORTB = ADCL;			// Show MSB/LSB (bit 10:0) of ADC
+		PORTA = ADCH;
+		wait(100);				// every 100 ms (busy waiting)
+	}
+}
+
+// Initialize ADC: 10-bits (left justified), free running
+void adcInit( void )
+{
+	ADMUX = 0b01100001;			// AREF=VCC, result left adjusted, channel1 at pin PF1
+	ADCSRA = 0b11100110;		// ADC-enable, no interrupt, start, free running, division by 64
+}
+
+void wait( int ms )
+{
+	for (int tms=0; tms<ms; tms++)
+	{
+		_delay_ms( 1 );			// library function (max 30 ms at 8MHz)
+	}
 }
