b3dbd2410bc482612f3b74599e5ec1030db040e0
E_Woods <we.baars@student.avans.nl>
2022-02-23 13:05:56 +0100

    opdrachtb2

--- Week4/Week4/main.c
+++ Week4/Week4/main.c
@@ -35,7 +35,7 @@ int main( void )
 // Initialize ADC: 10-bits (left justified), free running
 void adcInit( void )
 {
-	ADMUX = 0b01100001;			// AREF=VCC, result left adjusted, channel1 at pin PF1
+	ADMUX = 0b01100011;			// AREF=VCC, result left adjusted, channel1 at pin PF1
 	ADCSRA = 0b11100110;		// ADC-enable, no interrupt, start, free running, division by 64
 }
 
