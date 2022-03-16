2e2bd7ba5b0528b869490a138eaf4ecda7190860
Leon van der Sar <leon.v.d.sar@gmail.com>
2022-03-16 14:07:45 +0100

    OpdrachtB1b2

--- Week5/Week5/main.c
+++ Week5/Week5/main.c
@@ -92,7 +92,8 @@ void displayDriverInit()
 
   	spi_slaveSelect(0);				// Select dispaly chip
   	spi_write(0x0A);      			// Register 0A: Intensity
-  	spi_write(0x04);    			//  -> Level 4 (in range [1..F])
+  	//spi_write(0x04);				//  -> Level 4 (in range [1..F])
+	spi_write(0x0F);    			//  -> Level 7 (in range [1..F])
   	spi_slaveDeSelect(0);			// Deselect display chip
 
   	spi_slaveSelect(0);				// Select display chip
