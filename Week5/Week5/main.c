e103f315ae6e03f4a1bd6e9605e84f80be6b5f3b
Leon van der Sar <leon.v.d.sar@gmail.com>
2022-03-16 14:16:14 +0100

    OpdrachtB1c

--- Week5/Week5/main.c
+++ Week5/Week5/main.c
@@ -81,49 +81,39 @@ void spi_slaveDeSelect(unsigned char chipNumber)
 	PORTB |= BIT(chipNumber);
 }
 
+// Write a word = address byte + data byte from master to slave
+void spi_writeWord ( unsigned char adress, unsigned char data ) {
+		spi_slaveSelect(0);
+		spi_write(adress);
+		spi_write(data);
+		spi_slaveDeSelect(0);
+}
 
 // Initialize the driver chip (type MAX 7219)
 void displayDriverInit() 
 {
-	spi_slaveSelect(0);				// Select display chip (MAX7219)
-  	spi_write(0x09);      			// Register 09: Decode Mode
-  	spi_write(0xFF);				// 	-> 1's = BCD mode for all digits
-  	spi_slaveDeSelect(0);			// Deselect display chip
-
-  	spi_slaveSelect(0);				// Select dispaly chip
-  	spi_write(0x0A);      			// Register 0A: Intensity
-  	//spi_write(0x04);				//  -> Level 4 (in range [1..F])
-	spi_write(0x0F);    			//  -> Level 7 (in range [1..F])
-  	spi_slaveDeSelect(0);			// Deselect display chip
-
-  	spi_slaveSelect(0);				// Select display chip
-  	spi_write(0x0B);  				// Register 0B: Scan-limit
-	//spi_write(0x01); Wat er eerst stond, dit zorgt ervoor dat alleen digit 1 en 2 werken
-  	spi_write(0x03);   				// 	-> 1 = Display digits 0..1
-  	spi_slaveDeSelect(0);			// Deselect display chip
-
-  	spi_slaveSelect(0);				// Select display chip
-  	spi_write(0x0C); 				// Register 0C: Shutdown register
-  	spi_write(0x01); 				// 	-> 1 = Normal operation
-  	spi_slaveDeSelect(0);			// Deselect display chip
+	
+	spi_writeWord(0x09, 0xFF);
+	spi_writeWord(0x0A, 0x0F);
+	spi_writeWord(0x0B, 0x03);
+	spi_writeWord(0x0C, 0x01);
+	
 }
 
 // Set display on ('normal operation')
 void displayOn() 
 {
-  	spi_slaveSelect(0);				// Select display chip
-  	spi_write(0x0C); 				// Register 0B: Shutdown register
-  	spi_write(0x01); 				// 	-> 1 = Normal operation
-  	spi_slaveDeSelect(0);			// Deselect display chip
+	
+	spi_writeWord(0x0C, 0x01);
+	
 }
 
 // Set display off ('shut down')
 void displayOff() 
 {
-  	spi_slaveSelect(0);				// Select display chip
-  	spi_write(0x0C); 				// Register 0B: Shutdown register
-  	spi_write(0x00); 				// 	-> 1 = Normal operation
-  	spi_slaveDeSelect(0);			// Deselect display chip
+	
+	spi_writeWord(0x0C, 0x00);
+	
 }
 
 int main()
@@ -137,22 +127,19 @@ int main()
 	//for (char i =1; i<=2; i++) was 2, maar moest 4 zijn
 	for (char i =1; i<=4; i++)
 	{
-      	spi_slaveSelect(0); 		// Select display chip
-      	spi_write(i);  				// 	digit adress: (digit place)
-      	spi_write(0);			// 	digit value: 0 
-  	  	spi_slaveDeSelect(0);		// Deselect display chip
+		
+		spi_writeWord(i, 0);
+		
 	}    
 	wait(1000);
 
 	// write 4-digit data  
  	for (char i =1; i<=4; i++)
   	{
-		spi_slaveSelect(0);         // Select display chip
-		spi_write(i);         		// 	digit adress: (digit place)
-		spi_write(i);  		// 	digit value: i (= digit place)
-		spi_slaveDeSelect(0); 		// Deselect display chip
-	
+		  
+		spi_writeWord(i, i);
 		wait(1000);
+		
   	}
 	wait(1000);
 
