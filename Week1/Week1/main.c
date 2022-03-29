56fc97d84fa993348c5ef09c196dcb0fd8993699
Leon van der Sar <leon.v.d.sar@gmail.com>
2022-03-29 12:29:54 +0200

    Week 1 comments

--- Week1/Week1/main.c
+++ Week1/Week1/main.c
@@ -25,12 +25,12 @@ typedef struct {
 
 PATTERN_STRUCT pattern[] = {
 	{0x00, 100}, {0x01, 100}, {0x02, 100}, {0x04, 100}, {0x10, 100}, {0x20, 100}, {0x40, 100}, {0x80, 100}, //een kant op
-	{0x80, 100}, {0x40, 100}, {0x20, 100}, {0x10, 100}, {0x4, 100}, {0x02, 100}, {0x01, 100}, {0x00, 100}, //andere kant op
-	{0xAA,  50}, {0x55,  50},{0xAA,  50}, {0x55,  50},{0xAA,  50}, {0x55,  50},{0x00, 100}, //knipperen
-	{0x81, 100}, {0x42, 100}, {0x24, 100}, {0x18, 100}, // naar midden vanuit buiten
+	{0x80, 100}, {0x40, 100}, {0x20, 100}, {0x10, 100}, {0x4,  100}, {0x02, 100}, {0x01, 100}, {0x00, 100}, //andere kant op
+	{0xAA,  50}, {0x55,  50}, {0xAA,  50}, {0x55,  50}, {0xAA,  50}, {0x55,  50}, {0x00, 100}, //knipperen
+	{0x81, 100}, {0x42, 100}, {0x24, 100}, {0x18, 100}, //naar midden vanuit buiten
 	{0x18, 100}, {0x24, 100}, {0x42, 100}, {0x81, 100}, //naar buiten vanuit midden
-	{0x0F, 200}, {0xF0, 200}, {0x0F, 200}, {0xF0, 200}, // half om half
-	{0x00, 0x00}
+	{0x0F, 200}, {0xF0, 200}, {0x0F, 200}, {0xF0, 200}, //half om half
+	{0x00,0x00}
 };
 
 
@@ -38,16 +38,17 @@ PATTERN_STRUCT pattern[] = {
 
 int opdrachtb6(void)
 {
-		DDRD = 0b11111111;					// PORTD.7 input all other bits output
-		DDRC = 0b11111110;
+		DDRD = 0b11111111;					// PORTD all bits output
+		DDRC = 0b11111110;					// PORTC.0 input all other bits output
 		int buttonPressed = 1;
-		int fastCount = 0;
+		int fastCount = 0;					// Geeft aan in welke state het programma zit
 		int countTime = 1000;
 		int count = 0;
 		int countingUp = 1;
 		
 		while (1) 
 		{
+			
 			if (buttonPressed != (PINC & 0x01))
 			{
 				buttonPressed = PINC & 0x01;
@@ -59,21 +60,21 @@ int opdrachtb6(void)
 			}
 			
 			countTime = 1000;
-			if (fastCount)
+			if (fastCount) //Kijkt of de counter snel of niet snel moet tellen
 			{
 				countTime = 100;
 			}
 
-			if (count > countTime|| count < 0)
+			if (count > countTime|| count < 0) //Wanneer de counter 0 is of bij de grens aankomt dan de andere kant op gaan met tellen
 			{
 				countingUp = !countingUp;
 			}
 			
-			if (countingUp)
+			if (countingUp) //Kijkt of de counter op of af moet tellen
 			{
 				PORTD = 0x00;
 				count ++;
-			}else{
+			} else{
 				PORTD = 0x80;
 				count --;
 			}
@@ -85,8 +86,6 @@ int opdrachtb6(void)
 int opdrachtb5( void )
 {
 	DDRD = 0b11111111;					// PORTD all output
-	
-	
 	
 	while (1==1) {
 		// Set index to begin of pattern array
@@ -111,9 +110,9 @@ int opdrachtb2(void)
 	
 	while(1)
 	{
-		PORTD = 0x80;
+		PORTD = 0x80; //Zet een led aan
 		wait(250);
-		PORTD = 0x40;
+		PORTD = 0x40; //Zet een andere led aan
 		wait(250);
 	}
 	
@@ -123,17 +122,17 @@ int opdrachtb2(void)
 
 int opdrachtb3( void )
 {
-	DDRD = 0b11111111;					// PORTD.7 input all other bits output
-	DDRC = 0b11111110;
+	DDRD = 0b11111111;					// PORTD all bits output
+	DDRC = 0b11111110;					// PORTC.0 input all other bits output
 	
 	while (1) {
-		if (PINC & 0x1) {
-			PORTD = 0x80;
+		if (PINC & 0x1) {				//Check of de knop C.0 ingedrukt is
+			PORTD = 0x80;				//Knipperen
 			wait(250);
 			PORTD = 0x00;
 			wait(250);
 			} else {
-			PORTD = 0x00;				// write 0 to all the bits of PortD
+			PORTD = 0x00;				//Alle bits op PORTD op 0
 		}
 	}
 
@@ -143,13 +142,13 @@ int opdrachtb3( void )
 int opdrachtb4(void)
 {
 	DDRD = 0b11111111;
-	int increment = 1;
+	int increment = 1;					//Geeft aan of het looplicht naar links of rechts loopt
 	int value = 0x01;
 	
 	while(1)
 	{
 		
-		if (value == 0x80)
+		if (value == 0x80)				//Check of het looplicht bij een uiteinde is
 		{
 			increment = -1;
 		} else if (value == 0x01)
@@ -157,7 +156,7 @@ int opdrachtb4(void)
 			increment = 1;
 		}
 		
-		if (increment == 1)
+		if (increment == 1)			//Check welke kant het licht op moet lopen
 		{
 			value = value << 1;
 		} else
@@ -194,78 +193,77 @@ int main(void)
 }
 
 
- void Staart()
+ void Staart() //De start methode
 {
 	PORTC = 0b00000001;
 	
-	if (PIND & 0b01000000)
+	if (PIND & 0b01000000) //Check naar welke state er veranderd moet worden
 	{
 		currentState = S1;
 	}
 	
-	if (PIND & 0b00100000)
+	if (PIND & 0b00100000) //Check naar welke state er veranderd moet worden
 	{
 		currentState = S2;
 	}
 }
 
- void S1()
+ void S1() //State 1
 {
 	PORTC = 0b00000010;
 	
-	if (PIND & 0b10000000)
+	if (PIND & 0b10000000) //Check naar welke state er veranderd moet worden
 	{
 		currentState = Staart;
 	}
 	
-	if (PIND & 0b00100000)
+	if (PIND & 0b00100000) //Check naar welke state er veranderd moet worden
 	{
 		currentState = S2;
 	}
 
 }
 
- void S2()
+ void S2() //State 2
 {
 	PORTC = 0b00000100;
 	
-	if (PIND & 0b10000000)
+	if (PIND & 0b10000000) //Check naar welke state er veranderd moet worden
 	{
 		currentState = Staart;
 	}
 	
-	if (PIND & 0b01000000)
+	if (PIND & 0b01000000) //Check naar welke state er veranderd moet worden
 	{
 		currentState = S1;
 	}
 	
-	if (PIND & 0b00100000)
+	if (PIND & 0b00100000) //Check naar welke state er veranderd moet worden
 	{
 		currentState = S3;
 	}
 	
 }
 
- void S3()
+ void S3() //State 3
 {
 	PORTC = 0b00001000;
-	if (PIND & 0b10000000)
+	if (PIND & 0b10000000) //Check naar welke state er veranderd moet worden
 	{
 		currentState = Staart;
 	}
 	
-	if ((PIND & 0b01000000) || (PIND & 0b00100000))
+	if ((PIND & 0b01000000) || (PIND & 0b00100000)) //Check naar welke state er veranderd moet worden
 	{
 		currentState = End;
 	}
 	
-	
 }
 
- void End()
+ void End() //End state
 {
 	PORTC = 0b00010000;
-	if (PIND & 0b10000000)
+	if (PIND & 0b10000000) //Check naar welke state er veranderd moet worden
 	{
 		currentState = Staart;
 	}
